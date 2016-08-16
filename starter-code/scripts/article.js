616ffe2ff2d188e05adba481c25a8d63f334067c
Lee Broxson <broxsonl@gmail.com>
2016-08-16 14:52:28 -0700

    All TODO problems done. Lab finished. Struggled a bit on #3

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -13,9 +13,13 @@ function Article (opts) {
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
+  $newArticle.find('h1').text(this.title);
   $newArticle.attr('data-category', this.category);
+  $newArticle.find('.byline a').text(this.author);
+  $newArticle.find('.byline a').attr('href', this.authorUrl);
+  $newArticle.find('.article-body').html(this.body);
 
-  /* TODO: Now use jQuery to fill in the rest of the current
+  /* TODO: DONE! Now use jQuery to fill in the rest of the current
   template clone with properties from this particular Article instance.
   We need to fill in:
     1. author name,
@@ -24,14 +28,16 @@ Article.prototype.toHtml = function() {
     4. article body, and
     5. publication date. */
     // Display the date as a relative number of 'days ago'
-    $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
-    $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
 
-    /* TODO: This cloned article is no longer a template,
+    /* TODO: DONE! This cloned article is no longer a template,
     as it now has real data attached to it! We need to account
     for that before this current article gets rendered to our
     DOM. */
-    return $newArticle;
+
+  $newArticle.removeClass('template');
+  return $newArticle;
 };
 
 ourLocalData.sort(function(firstElement, secondElement) {
